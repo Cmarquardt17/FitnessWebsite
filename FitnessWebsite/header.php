625d4e9695d074a3de3946a15190a5ce0750a84f
Nayte Chandler <anaytor9@gmail.com>
2019-11-03 17:02:30 -0700

    added slideshow
    i added a slide show. used some javascript, its pretty neat.

--- FitnessWebsite/header.php
+++ FitnessWebsite/header.php
@@ -3,6 +3,7 @@ session_start();
 
 $clubstr = 'Fitness Bros';
 $sloganSTR ='Strive to Succeed';
+$sloganSTR2 = 'Welcome to the Pack!';
 $userstr = 'Welcome fellow lifter';
 
 echo <<<_INIT
@@ -15,6 +16,7 @@ echo <<<_INIT
         <link href="https://fonts.googleapis.com/css?family=Arsenal|Lora|Muli|Source+Sans+Pro|Playfair+Display&display=swap" rel="stylesheet">
         <link rel='stylesheet' href='css/styles.css'>
         <link rel='stylesheet' href='css/signupstyles.css'>
+        <link rel='stylesheet' href='css/headerstyle.css'>
         <title>$clubstr: $userstr</title>
         </head>
 _INIT;
@@ -33,31 +35,32 @@ echo <<<_HEADER_OPEN
     <body>
         <div id="wrapper">
         <header>
-            <img id = 'logoPic' src='img/wolf.png' alt='wolf' height='30' weight='30'>
-            <div id='logo'>$clubstr</div>
+            <img id = 'logoPic' src='img/wolf.png' alt='wolf' height='80' weight='30'>
+            <div id='logo'>$clubstr<div id='slogan'>$sloganSTR</div></div>
             <div>
-            <div id=slogan'>$sloganSTR</div>
 _HEADER_OPEN;
 
 if ($loggedin) {
 echo <<<_LOGGEDIN
 
             <nav><ul>
-                <li><a href='members.php?view=$user'>Home</a></li>
+                <li><a href='members.php?view=$user' onclick = "currentSlide(1)">Home</a></li>
                 <li><a href='members.php'>Members</a></li>
                 <li><a href='friends.php'>Friends</a></li>
                 <li><a href='messages.php'>Messages</a></li>
                 <li><a href='profile.php'>Edit Profile</a></li>
                 <li><a href='logout.php'>Log out</a></li>
+                <li><a href='about.php'>About</a></li>
             </ul></nav>
 _LOGGEDIN;
 } else {
 echo <<<_GUEST
 
             <nav><ul>
-                <li><a href='index.php'>Home</a></li>
+                <li><a href='index.php'onclick = "currentSlide(1)">Home</a></li>
                 <li><a href='signup.php'>Sign Up</a></li>
                 <li><a href='login.php'>Log In</a></li>
+                <li><a href='about.php'>About</a></li>
             </ul></nav>
 _GUEST;
  }
--- FitnessWebsite/index.php
+++ FitnessWebsite/index.php
@@ -5,11 +5,45 @@ require_once 'header.php';
 echo "<h3>Welcome to $clubstr. </h3>";
 echo "<div>";
 
-if ($loggedin) 
-    echo " $user, you are logged in";
-else           
-    echo 'Please sign up, or log in if you\'re already a member.';
+         
+echo <<<_END
+<!DOCTYPE html> 
+<html>
+<div class="slideshow-container">
 
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="img/pic1.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img/pic2.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img/pic3.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
+<script type='text/javascript'>window.onload=showSlides();</script>
+_END;
 echo <<<_END
     </div><br>
 _END;
