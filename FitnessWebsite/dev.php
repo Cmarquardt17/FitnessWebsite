14165e0d3126901ff2aecf83d9594be2e198301a
Zac Sliker <zasliker@gmail.com>
2019-11-19 16:42:04 -0700

    developers php file

---
+++ FitnessWebsite/dev.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+require_once 'header.php';
+echo "<img id = 'zac' src='img/zac.jpg' alt='Zac' height='180' weight='180'><p>This is Zac he may or may not have an extra chromozone were not sure/p>"
+?>
