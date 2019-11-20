d6e66c83a8181266362780a14253b4ce4abfe54b
Michael Muffoletto <muffmike1@gmail.com>
2019-11-20 14:03:41 -0700

    First Push
    Added blog page
    Updated rudimentary lines
    Added pictures

--- FitnessWebsite/header.php
+++ FitnessWebsite/header.php
@@ -52,12 +52,47 @@ echo <<<_LOGGEDIN
 _LOGGEDIN;
 } else {
 echo <<<_GUEST
+<<<<<<< Updated upstream
 
             <nav><ul>
                 <li><a href='index.php'>Home</a></li>
                 <li><a href='signup.php'>Sign Up</a></li>
                 <li><a href='login.php'>Log In</a></li>
             </ul></nav>
+=======
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #61b9fa;">
+  <a class="navbar-brand" href="#">
+  <img class='logo' src="img/ArticWolf.jpg" alt="logo" width="60" class="d-inline-block align-middle mr-2">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+    <li class="nav-item">
+      <a class="nav-link" href="dev.php">Developers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="blog.php">Blog</a>
+    </li>    
+    </ul>
+    <span class="navbar-text">
+    </span>
+  </div>
+</nav>
+>>>>>>> Stashed changes
 _GUEST;
  }
 
