5992fc033081d97db45a1fc546670436aa9f4731
Zac Sliker <zasliker@gmail.com>
2019-12-03 17:36:21 -0700

    updated devs page

--- FitnessWebsite/dev.php
+++ FitnessWebsite/dev.php
@@ -4,10 +4,10 @@ require_once 'header.php';
     $error = $user = $pass = "";
 echo<<<_END
 <div class ="container">
-    <h1>Meet Swole Team Six</h1>
+    <h1>Meet The Team</h1>
     <div>
         <h3><img id = 'Zac' src='img/zac.jpg' alt='Zac' height='300' weight='300'></h3>
-        <p class = "text-center">This is Zac, he may or may not have an extra chromosone were not sure.</p>
+        <p class = "text-center">This is Zac, he is in a fraternaty but still sucks at pong.</p>
         <h3><img id = 'Nayte' src='img/Nayte.jpg' alt='Nayte' height='350' weight='350'></h3>
         <p class = "text-center">This is Nayte, he party hards during the week and not on the weekends.</p>
         <h3><img id = 'Christian' src='img/Christian.jpg' alt='Christian' height='300' weight='300'></h3>
@@ -19,6 +19,8 @@ echo<<<_END
     <h1>&nbsp<h1>
     <h4 class = "text-center"> Apply to be a member of <strong>Swole Team Six</strong> email here:</h4>
     <h2 class ="text-center"> <strong> support@swolesix.com </strong> <h2>
+    <h3><img id = 'Map' src='img/map.jpg' alt='Map' height='400' weight='400'></h3>
+    <p class= "text-center">Located in Bozeman our Team is proud to have been providing websites for the last four years. However, if you do chose to work for us we offer the opportunity to work remotely. Why work for us? Swole Six prides itself with providing functional and efficient websites. We were voted the best company to get your foot into the profesional world of web development.</p>
     </div>
 </div>
 _END;
