aadb3f4168c6d3585a9af5e6907c8e35f9c1f224
Cmarquardt17 <christianmarquardt14@hotmail.com>
2019-12-05 17:53:10 -0700

    Push

--- FitnessWebsite/header.php
+++ FitnessWebsite/header.php
@@ -13,11 +13,12 @@ echo <<<_INIT
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Francois+One&display=swap" rel="stylesheet">
         <link rel='stylesheet' href='css/styles.css'>
         <link rel='stylesheet' href='css/signup.css'>
+        <link rel='stylesheet' href='css/LoginCss.css'>
         <title>$clubstr: $userstr</title>
         </head>
 _INIT;
@@ -33,23 +34,63 @@ else {
   $loggedin = FALSE;
   $userstr = '';
 }
+
+
+if (isset($_POST['user'])) {
+  $user = sanitizeString($_POST['user']);
+  $pass = sanitizeString($_POST['pass']);
+
+  if ($user == "" || $pass == "")
+      $error = 'Not all fields were entered';
+  else {
+      $result = queryMySQL("SELECT user,pass FROM members WHERE user='$user' AND pass='$pass'");
+
+      if ($result->num_rows == 0) {
+          $error = "Invalid login attempt";
+      }
+      else {
+          $_SESSION['user'] = $user;
+          $_SESSION['pass'] = $pass;
+          die("<h3>Welcome back, $user.</h3><p>Please <a href='members.php?view=$user'>click here</a> to continue.</p></div><footer></footer></body></html>");
+      }
+  }
+}
+
 echo <<<_HEADER_OPEN
         <body>
 _HEADER_OPEN;
 
 if ($loggedin) {
 echo <<<_LOGGEDIN
-
-            <nav class="mynav">
-            <ul>
-                <li><a href='members.php?view=$user'>Home</a></li>
-                <li><a href='members.php'>Members</a></li>
-                <li><a href='friends.php'>Friends</a></li>
-                <li><a href='messages.php'>Messages</a></li>
-                <li><a href='profile.php'>Edit Profile</a></li>
-                <li><a href='logout.php'>Log out</a></li>
-            </ul>
-            </nav>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #61b9fa;">
+  <a class="navbar-brand" href="http://localhost/FitnessWebsite/FitnessWebsite/index.php">
+  <img class='logo' src="img/ArticWolf.jpg" alt="logo" width="60" class="d-inline-block align-middle mr-2">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+    <li class="nav-item">
+      <a class="nav-link" href="dev.php">Developers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="blog.php">Blog</a>
+    </li>
+    </ul>
+    <span class="navbar-text">
+    </span>
+  </div>
+</nav>
 _LOGGEDIN;
 } else {
 echo <<<_GUEST
@@ -69,7 +110,7 @@ echo <<<_GUEST
         <a class="nav-link" href="signup.php">Sign Up</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
+      <a class="nav-link" href="login.php">Login</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="about.php">About</a>
--- FitnessWebsite/login.php
+++ FitnessWebsite/login.php
@@ -23,29 +23,47 @@ if (isset($_POST['user'])) {
 }
 
 echo <<<_END
+<div class="Cont">
 
-    <form method='post' action='login.php'>
-        <div data-role='fieldcontain'>
-            <label></label>
-            <span class='error'>$error</span>
-        </div>
-        <div data-role='fieldcontain'>
+    <button id="buttonLog" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+</div>
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="login.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="img/kitty.jpg" alt="Avatar" class="avatar">
+    </div>
+
+    <div data-role='fieldcontain'>
             <label></label>
             <h3>Enter username and password</h3>
         </div>
-        <div data-role='fieldcontain'>
-            <label>Username</label>
-            <input type='text' maxlength='16' name='user' value='$user'>
-        </div>
-        <div data-role='fieldcontain'>
-            <label>Password</label>
-            <input type='password' maxlength='16' name='pass' value='$pass'>
-        </div>
-        <div data-role='fieldcontain'>
-            <label></label>
-            <input data-transition='slide' type='submit' value='Login'>
-        </div>
-    </form>
+    <div data-role='fieldcontain'>
+        <label style="color: black; margin-left: 15px;">Username</label>
+        <input type='text' maxlength='16' name='user' value='$user'>
+    </div>
+    <div data-role='fieldcontain'>
+        <label style="color: black; margin-left: 15px;">Password</label>
+        <input type='password' maxlength='16' name='pass' value='$pass'>
+    </div>
+    <div data-role='fieldcontain'>
+    <label></label>
+    <input data-transition='slide' type='submit' value='Login'>
+</div>
+    </div>
+</div>
+    <script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 _END;
 require_once 'footer.php';
 ?>
